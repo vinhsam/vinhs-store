36a850f55179be9a2277f8679593b87e97388009
Vinh Sam <quailbert@gmail.com>
2018-10-15 18:38:48 +0000

    button hover style

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -6,6 +6,11 @@ const Button = styled.button`
     border-radius: 3px;
     color: white;
     padding: 5px 10px;
+    transition: background 0.25s;
+
+    &:hover {
+        background: darkorange;
+    }
 `;
 
 export default Button;
